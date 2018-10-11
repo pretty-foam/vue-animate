6aef694d93f451aed9c6b1a9f4c6257282f3d5b1
prettyfoam <1334875424@QQ.com>
2018-10-11 15:54:41 +0800

    Update vue-animate.js

--- vue-animate.js
+++ vue-animate.js
@@ -1,4 +1,3 @@
-
 //函数防抖
 const debounce = (func, wait) => {
     let timer = null
@@ -15,7 +14,7 @@ const debounce = (func, wait) => {
 //判断元素是否可见
 const see =(el,binding)=>{
     const offsetHeight = el.getBoundingClientRect().top //滚动距离游览器可视顶部高度
-    const height = el.getBoundingClientRect().height //元素高度
+    const height = el.getBoundingClientRect().height || el.height //元素高度
     const wh = window.innerHeight //内容视口高度
     const animateClassName = binding.value || '' //动画类名
     const className = el.className           //当前类名
@@ -23,14 +22,14 @@ const see =(el,binding)=>{
     //判读初始元素是否可见，可见添加动画类名
     if(offsetHeight<=wh&&offsetHeight>=-height){
         el.style.visibility='visible'
-        el.className = className +'' +animateClassName
+        el.className = className +' ' +animateClassName
     }
         //监听滚动判读是否可见，可见执行动画，不可见则隐藏
    window.addEventListener('scroll',debounce(()=>{
         const offsetHeight = el.getBoundingClientRect().top //滚动距离游览器可视顶部高度
         if(offsetHeight<=wh&&offsetHeight>=-height){
             el.style.visibility='visible'
-            el.className = className +'' +animateClassName
+            el.className = className +' ' +animateClassName
         }else{
             el.className = className
             el.style.visibility='hidden'
